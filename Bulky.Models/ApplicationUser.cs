f96db3c4c23f8f877c73e0ab7677cc98b33ae93f
jol96 <jack1996oloughlin@gmail.com>
2023-05-05 10:28:58 +0100

    Section 15: 188 Dispaly user list

--- Bulky.Models/ApplicationUser.cs
+++ Bulky.Models/ApplicationUser.cs
@@ -15,12 +15,15 @@ namespace BulkyBook.Models
         [Required]
         public string Name { get; set; }
 
-        public string? StreetAddress{ get; set; }
+        public string? StreetAddress { get; set; }
         public string? City { get; set; }
-        public string? State{ get; set; }
+        public string? State { get; set; }
         public string? PostalCode { get; set; }
+        public int? CompanyId { get; set; }
         [ForeignKey("CompanyId")]
         [ValidateNever]
-        public int? CompanyId { get; set; }
+        public Company? Company { get; set; }
+        [NotMapped]
+        public string Role { get; set; }
     }
 }
